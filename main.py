bbda589a98e599162e760485dbb633296a5cdd5b
cloud <ali.nuranoff@gmail.com>
2023-08-24 17:39:57 +0500

    first

---
+++ main.py
@@ -0,0 +1,7 @@
+def num():
+    while True:
+        numbers = int(input('vvedi'))
+        if numbers %2 == 0:
+            print('cotnoe')
+        elif numbers %2 != 0:
+            print('necetnoe')
